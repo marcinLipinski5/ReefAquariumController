50cbe4ede2d571bffd733a0d8a166a75df886ec4
rpi3b <marcin.lipinski5@gmail.com>
2022-08-30 13:28:04 +0200

    live connect changes

--- autoRefill/controller.py
+++ autoRefill/controller.py
@@ -12,29 +12,40 @@ class Controller:
 
     def __init__(self, database=TinyDB('db.json')):
         self.database = database.table("auto_refill")
-        # self.database.insert({'type': 'alarm', 'status': False})
-        # self.database.insert({'type': 'daily_refill_flow', 'flow': 0})
-        # self.database.insert({'type': 'max_daily_refill_flow', 'flow': 1000})
+        self.database.insert({'type': 'alarm', 'status': False})
+        self.database.insert({'type': 'daily_refill_flow', 'flow': 0})
+        self.database.insert({'type': 'max_daily_refill_flow', 'flow': 1000})
 
+        GPIO.setmode(GPIO.BCM)
         self.water_level_sensor_down_value_main = IOPins.WATER_LEVEL_SENSOR_DOWN_VALUE_MAIN.value
         self.water_level_sensor_down_value_backup = IOPins.WATER_LEVEL_SENSOR_DOWN_VALUE_BACKUP.value
         self.water_level_sensor_up_value = IOPins.WATER_LEVEL_SENSOR_UP_VALUE.value
         self.water_pump_refill_relay = IOPins.WATER_PUMP_REFILL_RELAY.value
-        self.water_pump_refill_flow_control = IOPins.WATER_PUMP_REFILL_FLOW_COUNTER.value
+        # self.water_pump_refill_flow_control = IOPins.WATER_PUMP_REFILL_FLOW_COUNTER.value
+        GPIO.setup(self.water_level_sensor_down_value_main, GPIO.IN)
+        GPIO.setup(self.water_level_sensor_down_value_backup, GPIO.IN)
+        GPIO.setup(self.water_level_sensor_up_value, GPIO.IN)
+        GPIO.setup(self.water_pump_refill_relay, GPIO.OUT, initial=GPIO.LOW)
+        # GPIO.setup(self.water_pump_refill_flow_control, GPIO.IN)
 
         self.alarm = self.get_alarm()
 
     def run(self):
-        time_counter = 5  #  max time for pump to run in single iteration
+        time_counter = 10  #  max time for pump to run in single iteration
+        print("checking conditions")
         self.check_alarm_conditions()
         if self.should_pump_be_active():
+            print("activate pump")
             GPIO.output(self.water_pump_refill_relay, GPIO.HIGH)
             time.sleep(time_counter)
             GPIO.output(self.water_pump_refill_relay, GPIO.LOW)
+            print("deactivate pump")
 
     @staticmethod
     def check_level_sensor_state(sensor_pin: int) -> bool:  # TODO adjust to NC/NO sensor structure
-        return True if GPIO.input(sensor_pin) == 1 else False
+        state = True if GPIO.input(sensor_pin) == 1 else False
+        print(f"Sensor state on pin: {sensor_pin} -> {state}")
+        return state
         # return False
 
     def check_alarm_conditions(self) -> None:
@@ -66,7 +77,7 @@ class Controller:
         self.alarm = alarm_state
         if self.alarm:
             logging.warning(f"Alarm level for auto refill reached. "
-                            f"UP_LEVEL_SENSOR_STATUS: {self.check_level_sensor_state(self.water_level_sensor_up_value)}, "
+                            f"UP_LEVEL_SENSOR_STATUS: ??, "
                             f"DAILY_REFILL_FLOW: {self.database.get(Query().type == 'daily_refill_flow')['flow']}")
 
     def get_alarm(self) -> bool:
@@ -74,10 +85,9 @@ class Controller:
 
 if __name__ == "__main__":
     dupa = Controller()
-    # dupa.set_alarm(False)
-    dupa.run()
-    print(dupa.get_alarm())
-    # dupa.set_alarm(False)
-    # print(dupa.get_alarm())
-    # print(dupa.is_daily_water_flow_reached())
+    while True:
+        dupa.run()
+        print("-------------------------")
+        time.sleep(5)
+        
 
