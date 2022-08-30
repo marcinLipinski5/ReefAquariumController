bf4c6a514793d8e8a25bd9ef6a949c369fd151d5
marcin <marcin.lipinski5@gmail.com>
2022-08-30 14:21:50 +0200

    add support for flow sensor

--- autoRefill/controller.py
+++ autoRefill/controller.py
@@ -5,8 +5,9 @@ import traceback
 import RPi.GPIO as GPIO
 from tinydb import TinyDB, Query
 
+from autoRefill.flow_counter import FlowCounter
 from pins.IOPins import IOPins
-
+import signal
 
 class Controller:
 
@@ -21,32 +22,39 @@ class Controller:
         self.water_level_sensor_down_value_backup = IOPins.WATER_LEVEL_SENSOR_DOWN_VALUE_BACKUP.value
         self.water_level_sensor_up_value = IOPins.WATER_LEVEL_SENSOR_UP_VALUE.value
         self.water_pump_refill_relay = IOPins.WATER_PUMP_REFILL_RELAY.value
-        # self.water_pump_refill_flow_control = IOPins.WATER_PUMP_REFILL_FLOW_COUNTER.value
+
         GPIO.setup(self.water_level_sensor_down_value_main, GPIO.IN)
         GPIO.setup(self.water_level_sensor_down_value_backup, GPIO.IN)
         GPIO.setup(self.water_level_sensor_up_value, GPIO.IN)
         GPIO.setup(self.water_pump_refill_relay, GPIO.OUT, initial=GPIO.LOW)
-        # GPIO.setup(self.water_pump_refill_flow_control, GPIO.IN)
 
         self.alarm = self.get_alarm()
 
     def run(self):
         time_counter = 10  #  max time for pump to run in single iteration
-        print("checking conditions")
         self.check_alarm_conditions()
         if self.should_pump_be_active():
-            print("activate pump")
-            GPIO.output(self.water_pump_refill_relay, GPIO.HIGH)
-            time.sleep(time_counter)
-            GPIO.output(self.water_pump_refill_relay, GPIO.LOW)
-            print("deactivate pump")
+            logging.info("Running auto refill pump")
+            try:
+                signal.signal(signal.SIGALRM, self.signal_handler)
+                signal.alarm(time_counter - 1)
+                try:
+                    GPIO.output(self.water_pump_refill_relay, GPIO.HIGH)
+                    FlowCounter().run(time_counter)
+                except:
+                    logging.error("Time out for refill pump!")
+                finally:
+                    GPIO.output(self.water_pump_refill_relay, GPIO.LOW)
+            except:
+                logging.error("An error occurs during refill pump switching process")
+            finally:
+                GPIO.output(self.water_pump_refill_relay, GPIO.LOW)
 
     @staticmethod
     def check_level_sensor_state(sensor_pin: int) -> bool:  # TODO adjust to NC/NO sensor structure
         state = True if GPIO.input(sensor_pin) == 1 else False
         print(f"Sensor state on pin: {sensor_pin} -> {state}")
         return state
-        # return False
 
     def check_alarm_conditions(self) -> None:
         alarm_state = True
@@ -77,11 +85,16 @@ class Controller:
         self.alarm = alarm_state
         if self.alarm:
             logging.warning(f"Alarm level for auto refill reached. "
-                            f"UP_LEVEL_SENSOR_STATUS: ??, "
+                            f"UP_LEVEL_SENSOR_STATUS: {self.check_level_sensor_state(self.water_level_sensor_up_value)}, "
                             f"DAILY_REFILL_FLOW: {self.database.get(Query().type == 'daily_refill_flow')['flow']}")
 
     def get_alarm(self) -> bool:
         return self.database.get(Query().type == 'alarm')['status']
+
+    @staticmethod
+    def signal_handler(signum, frame):
+        raise Exception("Timed out!")
+
 
 if __name__ == "__main__":
     dupa = Controller()
--- autoRefill/flow_counter.py
+++ autoRefill/flow_counter.py
@@ -1,5 +1,9 @@
 from datetime import date
 from tinydb import TinyDB, Query
+import RPi.GPIO as GPIO
+import time, sys
+
+from pins.IOPins import IOPins
 
 
 class FlowCounter:
@@ -7,29 +11,43 @@ class FlowCounter:
     #  TODO run it as a separete thread on start of Main(). Purpose for this class is only to count flow, save to db and reset state on midnight
     def __init__(self, database=TinyDB('db.json')):
         self.database = database.table("auto_refill")
-        # self.database.insert({'type': 'flow_count_date', 'date': "29-08-2022"})
+        self.database.insert({'type': 'flow_count_date', 'date': "29-08-2022"})
 
-    def run(self):
-        # while True:
-            # collect data
-            # if data != 0:
-              if self.should_daily_refill_counter_be_reset(): self.reset_counter()
-            #  calculate water_amount
-            #  amount_in_database += water_amount
-            # pass
+        self.pulse_counter = 0
 
-    def should_daily_refill_counter_be_reset(self):
-        return self.database.get(Query().type == 'flow_count_date')['date'] != self.get_current_date()
+        self.water_pump_refill_flow_counter = IOPins.WATER_PUMP_REFILL_FLOW_COUNTER.value
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.water_pump_refill_flow_counter, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def reset_counter(self):
+    def run(self, time_of_refill: int):
+        print(f"COUNTER IN: {self.pulse_counter}")
+        if self.__should_daily_refill_counter_be_reset(): self.__reset_counter()
+        GPIO.add_event_detect(self.water_pump_refill_flow_counter, GPIO.RISING, callback=self.__count_pulse)
+        time.sleep(time_of_refill)
+        GPIO.remove_event_detect(self.water_pump_refill_flow_counter)
+        print(f"COUNTER OUT: {self.pulse_counter}")
+        self.database.update({'flow': self.__get_flow_in_milliliters()}, Query().type == 'daily_refill_flow')
+
+    def __get_flow_in_milliliters(self) -> float:
+        # TODO some calculations here
+        result = self.pulse_counter * 2
+        return result
+
+    def __count_pulse(self):
+        self.pulse_counter += 1
+
+    def __should_daily_refill_counter_be_reset(self):
+        return self.database.get(Query().type == 'flow_count_date')['date'] != self.__get_current_date()
+
+    def __reset_counter(self):
         self.database.update({'flow': 0}, Query().type == 'daily_refill_flow')
-        self.database.update({'date': self.get_current_date()}, Query().type == 'flow_count_date')
+        self.database.update({'date': self.__get_current_date()}, Query().type == 'flow_count_date')
 
     @staticmethod
-    def get_current_date():
+    def __get_current_date():
         return date.today().strftime("%d-%m-%Y")
 
 if __name__ == "__main__":
     dupa = FlowCounter()
-    print(dupa.should_daily_refill_counter_be_reset())
-    dupa.run()
+    print(dupa.__should_daily_refill_counter_be_reset())
+    dupa.__run()
